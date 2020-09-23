8019353834570c8fe281c12460581bc4ec079061
lorennalehu <showbells0702gmail.com>
2020-09-22 20:36:13 -0500

    CAMBIO

--- src/data.js
+++ src/data.js
@@ -1,30 +1,34 @@
-import data from './data/pokemon/pokemon.js';
-constArrayPokemons=Object.values(pokemon.data);
-//filtrar tipos
+export const ordenar ={
+  az:function(pokemon){
+  return pokemon.sort((pokemonA,pokemonB)=>{
+      if(pokemonA.name > pokemonB.name){
+          return 1;
+      }
+      if(pokemonA.name < pokemonB.name){
+          return -1;
+      }
+      return 0;
+   });
+  },
+  za:function(pokemon){
+  return pokemon.sort((pokemonA,pokemonB)=>{
+      if(pokemonA.name < pokemonB.name){
+          return 1;
+      }
+      if(pokemonA.name > pokemonB.name){
+          return -1;
+      }
+      return 0;
+   });
+  },
+};
 
-//ordenar de A-Z
-
-const ordenarAz=newArrayPokemons.sort((pokemonA,pokemonB)=>{
-    if(pokemonA.name < pokemonB.name){
-        return 1;
-    }
-    if(pokemonA.name > pokemonB.name){
-        return -1;
-    }
-    return 0;
-})
-console.log(ordenarAz);
-//ORDENAR DE Z-A
-
-const ordenarZa=newArrayPokemonsReverse.sort((pokemonA,pokemonB)=>{
-    if(pokemonA.name > pokemonB.name){
-        return 1;
-    }
-    if(pokemonA.name < pokemonB.name){
-        return -1;
-    }
-    return 0;
-})
-console.log(ordenarZa);
-
- export default data;
+export const filtrarGeneracion={
+  kanto:function(pokemon , generation){
+   const pkanto= pokemones.filter((pokemon) => {
+     if(generation === "kanto"){
+      return pkanto;
+      } 
+    });
+  },
+};
--- src/main.js
+++ src/main.js
@@ -1,138 +1,6 @@
-import data from './data/pokemon/pokemon.js';
 
-//click kanto
-document.getElementById('kanto').addEventListener('click',function(){
-    //ocutar pantalla 1
-    document.getElementById('pantalla1').style.display='none';
-    //mostrar pantalla 2
-    document.getElementById('pantallaKanto').style.display = 'block';
-});
-//click johto
-document.getElementById('johto').addEventListener('click',function(){
-    //ocutar pantalla 1
-    document.getElementById('pantalla1').style.display='none';
-    //mostrar pantalla 2
-    document.getElementById('pantallaJohto').style.display = 'block';
-});
-//click al boton tipos
-document.getElementById('tipos').addEventListener('click',function(){
-    //ocutar pantalla 1
-    document.getElementById('pantalla1').style.display='none';
-    //mostrar pantalla 2
-    document.getElementById('pantallaTipos').style.display = 'block';
-});
-//click botones de tipo de pokemon agua
-document.getElementById('water').addEventListener('click',function(){  
-document.getElementById('pantallaTipos').style.display='none';
-document.getElementById('pantallaFinal').style.display = 'block';    
-})
-//click botones de tipo de pokemon normal
-document.getElementById('normal').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon lucha
-document.getElementById('fighting').addEventListener('click',function(){ 
-console.log('asdasdasd'); 
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon fuego
-document.getElementById('fire').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon planta
-document.getElementById('grass').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon hielo
-document.getElementById('ice').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon roca
-document.getElementById('rock').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon veneno
-document.getElementById('poison').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon tierra
-document.getElementById('earth').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon electrico
-document.getElementById('electric').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon psiquico
-document.getElementById('psychic').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon bicho
-document.getElementById('bug').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon volador
-document.getElementById('flying').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon siniestro
-document.getElementById('sinester').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon acero
-document.getElementById('steel').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon hada
-document.getElementById('fairy').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon fantasma
-document.getElementById('ghost').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click botones de tipo de pokemon dragon
-document.getElementById('dragon').addEventListener('click',function(){  
-    document.getElementById('pantallaTipos').style.display='none';
-    document.getElementById('pantallaFinal').style.display = 'block';    
-    })
-//click al menu CP
-document.getElementById('cp').addEventListener('click',function(){
-    document.getElementById('pantalla1').style.display='none';
-    document.getElementById('pantallaCp').style.display = 'block';
-});
-//click de a - z
-document.getElementById('az').addEventListener('click',function(){
-    alert("hola");
-});
-//click de z - a
-document.getElementById('za').addEventListener('click',function(){
-    alert("hola");
-});
-//click de mayor a menor cp
-document.getElementById('mame').addEventListener('click',function(){
-    alert("hola");
-});
-//click de menos a mayor cp
-document.getElementById('mema').addEventListener('click',function(){
-    alert("hola");
-});
+import { ordenar , filtrarGeneracion} from'./data.js';
+import data from './data/pokemon/pokemon.js';
  //jalando data
  function pokemonDetalle(pokemonsResultado) {
     const imagenes = pokemonsResultado.map((pokemon,i)=>{
@@ -143,21 +11,135 @@ document.getElementById('mema').addEventListener('click',function(){
         let informacion = pokemon.name ;
        return informacion  ;
     });
+    const generation = pokemonsResultado.map((pokemon, i) => {
+        let informacion = pokemon.generation.name ;
+       return informacion  ;
+    });   
+    const stats = pokemonsResultado.map((pokemon, i) => {
+        let informacion = pokemon.stats['max-cp'];
+       return informacion  ;
+    });
 
-    
-    for (let j=0 ; j<250 ; j++){
+   
+     for (let j=0 ; j<pokemonsResultado.length ; j++){    
         let pokemon =document.createElement("div");
-        pokemon.classList.add("cardPokemon"); 
-        pokemon.innerHTML = "<img src='" + imagenes[j] +"'/><h3>"+ names[j] +"</h3>";
-        document.getElementById('pantallaCp').appendChild(pokemon);
+        pokemon.innerHTML = `
         
-    }
-    
-}
- 
-// pokemonDetalle(data.pokemon);
-// console.table(data.pokemon);
+         <div class='front'>
+            <img src='${imagenes[j]}'/>
+            <h3>"${names[j]}"</h3>  
+         </div>
+         <div class='back'>
+           <h3>REGIÓN: ${generation[j]}</h3> 
+           <h3>MAX-CP: ${stats[j]}</h3>  
+         </div>
+         `
 
+document.getElementById('pantalla1').appendChild(pokemon);  
+    } 
+ }
+pokemonDetalle(data.pokemon);
+console.table(data.pokemon);
+
+//click al boton tipos
+document.getElementById('tipos').addEventListener('click',function(){
+    //ocutar pantalla 1
+    document.getElementById('pantalla1').style.display='none';
+    //mostrar pantalla 2
+    document.getElementById('pantallaTipos').style.display = 'block';
+});
+//click botones de tipo de pokemon agua
+document.getElementById('water').addEventListener('click',function(){  
+    document.getElementById('pantallaTipos').style.display='none';
+    document.getElementById('pantallaFinal').style.display = 'block';    
+    })
+    //click botones de tipo de pokemon normal
+    document.getElementById('normal').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon lucha
+    document.getElementById('fighting').addEventListener('click',function(){ 
+    console.log('asdasdasd'); 
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon fuego
+    document.getElementById('fire').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon planta
+    document.getElementById('grass').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon hielo
+    document.getElementById('ice').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon roca
+    document.getElementById('rock').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon veneno
+    document.getElementById('poison').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon tierra
+    document.getElementById('earth').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon electrico
+    document.getElementById('electric').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon psiquico
+    document.getElementById('psychic').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon bicho
+    document.getElementById('bug').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon volador
+    document.getElementById('flying').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon siniestro
+    document.getElementById('sinester').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon acero
+    document.getElementById('steel').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon hada
+    document.getElementById('fairy').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon fantasma
+    document.getElementById('ghost').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    //click botones de tipo de pokemon dragon
+    document.getElementById('dragon').addEventListener('click',function(){  
+        document.getElementById('pantallaTipos').style.display='none';
+        document.getElementById('pantallaFinal').style.display = 'block';    
+        })
+    
 //filtrar tipos
 function filterType(event){
     console.log(event.target.id)
@@ -176,7 +158,6 @@ function filterType(event){
                 let pokemon_type= character.type;
                 let pokemon_print =document.createElement("div");
                 pokemon_print.setAttribute('class', 'box_type');
-                // pokemon_print.innerHTML += "<div class='front'><img src='" + pokemon_img +"'/><h3>"+ pokemon_name +"</h3></div><div class='back'><h3>"+ "REGIÓN:"+''+ pokemon_generation +"</h3><h3>"+"TIPO:"+''+ pokemon_type +"</h3><h3>"+"HUEVOS:"+''+ pokemon_egg +"</h3></div>";
                 pokemon_print.innerHTML = `
                 <div class='front'>
                     <img src='${pokemon_img}'/>
@@ -201,3 +182,31 @@ for(let i = 0; i < buttons.length; i++ ){
     buttons[i].addEventListener('click', filterType) //2 parametros un evento y una funcion
 }
 
+//click a ordenar az
+document.getElementById('az').addEventListener('click',function(){
+//ordenar data
+let ordenAz = ordenar.az(data.pokemon);
+//mostrar en pantalla
+document.getElementById('pantalla1').innerHTML="";
+//ordenado
+pokemonDetalle(ordenAz);
+});
+
+//click a ordenar za
+document.getElementById('za').addEventListener('click',function(){
+//ordenar data
+let ordenZa = ordenar.za(data.pokemon);
+//mostrar en pantalla
+document.getElementById('pantalla1').innerHTML="";
+//ordenado
+pokemonDetalle(ordenZa);
+});
+//click a kanto
+document.getElementById('kanto').addEventListener('click',function(){
+//filtrar data
+let filtrado = filtrarGeneracion.kanto(data.pokemon);
+//mostrar en pantalla
+document.getElementById('pantalla1').innerHTML="";
+//filtrado
+pokemonDetalle(filtrado);
+});
